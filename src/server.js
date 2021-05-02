1ba924fcbcb4666d312827a2ee47377e4945c6ad
JúDCraide <juliadcraide31@gmail.com>
2021-05-02 16:15:59 -0300

    config: Config basic server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes')
+
+const app = express();
+const server = require('http').Server(app);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_DATA_BASE}?retryWrites=true&w=majority`, 
+    { useUnifiedTopology: true,useNewUrlParser: true  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
